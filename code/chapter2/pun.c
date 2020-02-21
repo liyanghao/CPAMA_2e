479e5244503017ce7409968214bc2924bb056bc4
LiyangHao <liyanghao19890724@163.com>
2020-02-21 23:10:36 +0800

    Create pun.c

---
+++ code/chapter2/pun.c
@@ -0,0 +1,17 @@
+/* 
+ * Name: pun.c
+ * Purpose: Prints a bad pun.
+ * Author: K. N. King 		 
+ */
+
+// Name: pun.c
+// Purpose: Prints a bad pun.
+// Author: K. N. King
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("To C, or not to C: that is the questions\n");
+	return 0;
+}
