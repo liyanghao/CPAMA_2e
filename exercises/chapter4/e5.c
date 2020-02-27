f3d496df93fb1a76c0febdfd9a7cdb09cfef9f81
LiyangHao <liyanghao19890724@163.com>
2020-02-27 22:41:19 +0800

    Create e5.c

---
+++ exercises/chapter4/e5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i1, j1;
+
+	i1 = 8;
+	j1 = 5;
+	printf("%d\n", i1%j1);
+
+	int i2, j2;
+
+	i2 = -8;
+	j2 = 5;
+	printf("%d\n", i2%j2);
+	
+	int i3, j3;
+
+	i3 = 8;
+	j3 = -5;
+	printf("%d\n", i3%j3);
+
+	int i4, j4;
+
+	i4 = -8;
+	j4 = -5;
+	printf("%d\n", i4%j4);
+
+	return 0;
+}
