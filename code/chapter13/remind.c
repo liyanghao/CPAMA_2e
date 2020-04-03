5cd25af25e2f328d7469ff351bca7325e48ccd96
Liyanghao <liyanghao19890724@163.com>
2020-04-04 07:32:11 +0800

    Update remind.c

--- code/chapter13/remind.c
+++ code/chapter13/remind.c
@@ -5,7 +5,24 @@
 #define MSG_LEN 60
 
 int read_line(char str[], int n);
-
+// 输出一个月的提醒列表
+// 输出示例：
+// Enter day and reminder: 24 Susan's birthday
+// Enter day and reminder: 5 6:00 - Dinner with Marge and Russ
+// Enter day and reminder: 26 Movie - "Chinatown" 
+// Enter day and reminder: 7 10:30 - Dental appointment
+// Enter day and reminder: 12 Movie - "Dazed and Confused"
+// Enter day and reminder: 5 Saturday class
+// Enter day and reminder: 12 Saturday class
+// Enter day and reminder: 0
+// Day  Reminder
+// 5  Saturday class
+// 5  6:00 - Dinner with Marge and Russ
+// 7  10:30 - Dental appointment
+// 12  Saturday class
+// 12  Movie - "Dazed and Confused"
+// 24  Susan's birthday
+// 26  Movie - "Chinatown"
 int main(void)
 {
     char reminders[MAX_REMIND][MSG_LEN+3];
