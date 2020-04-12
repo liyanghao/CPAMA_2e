585ce8118cbd65d3627f886cae346e3ae263ab07
LiyangHao <liyanghao19890724@163.com>
2020-04-12 20:32:06 +0800

    Create e2.c

---
+++ exercises/chapter2/e2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/*
+ * Copyright: 2020 by Author Liyang Hao
+ * File name: e2.c
+ * Description:《C语言：一种现代方法》第二章练习题2 
+ * Author: Liyang Hao
+ * Date: 2020/04/12
+ * 
+ * 编译指令：
+ * gcc -Wall -std=c89 -o e2 e2.c
+ * gcc -Wall -std=c99 -o e2 e2.c
+ * 该程序的输出为：
+ * Parkinson's Law:
+ * Work expands so as tofill the time
+ * available for the completion.
+ */
+int main()
+{
+	printf("Parkinson's Law:\nWork expands so as to");
+	printf("fill the time\n");
+	printf("available for the completion.\n");
+	return 0;
+}
