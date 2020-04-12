bd930e60408654f15931ddd8ffe64f708dd6972d
LiyangHao <liyanghao19890724@163.com>
2020-04-12 20:32:04 +0800

    Create e1.c

---
+++ exercises/chapter2/e1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+ * Copyright: 2020 by Author Liyang Hao
+ * File name: e1.c
+ * Description:《C语言：一种现代方法》第二章练习题1
+ * Author: Liyang Hao
+ * Date: 2020/04/12
+ * 
+ * 编译指令：
+ * gcc -Wall -std=c89 -o e1 e1.c
+ * gcc -Wall -std=c99 -o e1 e1.c
+ */
+int main(void)
+{
+  printf("hello, world\n");
+}
