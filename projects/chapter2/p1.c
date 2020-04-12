b5f6ca1831d7b5e9032cb85bd558bd350f542a3f
LiyangHao <liyanghao19890724@163.com>
2020-04-12 20:32:25 +0800

    Create p1.c

---
+++ projects/chapter2/p1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("%8s\n", "*");
+	printf("%7s\n", "*");
+	printf("%6s\n", "*");
+	printf("%s%4s\n", "*", "*");
+	printf("%2s%2s\n", "*", "*");
+	printf("%3s\n", "*");
+
+    return 0;
+}
