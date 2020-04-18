79a954410b5f43facbe848eaff2ee93af4ed172a
LiyangHao <liyanghao19890724@163.com>
2020-04-18 22:10:38 +0800

    Create e15.c

---
+++ exercises/chapter4/e15.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i= 1, j = 2;
+
+	i += j;
+
+	printf("%d %d\n", i, j);
+
+	i--;
+
+	printf("%d %d\n", i, j);
+
+	i * j / i;
+
+	printf("%d %d\n", i, j);
+
+	i % ++j;
+
+	printf("%d %d\n", i ,j);
+
+	return 0;
+}
