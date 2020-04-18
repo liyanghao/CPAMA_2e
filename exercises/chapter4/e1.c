de2c1b302e7d4afb7237b66f111a530f96b8a5af
LiyangHao <liyanghao19890724@163.com>
2020-04-18 09:38:45 +0800

    Update e1.c

--- exercises/chapter4/e1.c
+++ exercises/chapter4/e1.c
@@ -3,10 +3,16 @@
 int main(void)
 {
 	int i1 = 5, j1 = 3;
-	printf("%d %d", i1/j1, i1%j1);
+	printf("%d %d\n", i1/j1, i1%j1);
 
 	int i2 = 2, j2 = 3;
-	printf("%d\n", (i2+10)/j2);
+	printf("%d\n", (i2+10)%j2);
+
+	int i3 = 7, j3 = 8, k3 = 9;
+	printf("%d\n", (i3+10)%k3/j3);
+
+	int i4 = 1, j4 = 2, k4 = 3;
+	printf("%d\n", (i4+5)%(j4+2)/k3);
 
 	return 0;
 }
