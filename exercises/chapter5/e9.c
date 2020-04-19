8184dbee3d799257bd2dfa7d805d48869577b10c
LiyangHao <liyanghao19890724@163.com>
2020-04-19 08:03:45 +0800

    Create e9.c

---
+++ exercises/chapter5/e9.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void)
+{
+	int score;
+
+	printf("Enter the score: ");
+	scanf("%d", &score);
+
+	if (score >= 90) {
+		printf("A\n");
+	}else if (score >= 80) {
+		printf("B\n");
+	}else if (score >= 70) {
+		printf("C\n");
+	}else if (score >= 60) {
+		printf("D\n");
+	}else {
+		printf("F\n");
+	}
+
+	if (score < 60) {
+		printf("F\n");
+	}else if (score < 70) {
+		printf("D\n");
+	}else if (score < 80) {
+		printf("C\n");
+	}else if (score < 90) {
+		printf("B\n");
+	}else {
+		printf("A\n");
+	}
+
+
+	return 0;
+}
