084062b9722d7f3c90091fc33354943adcc2b096
LiyangHao <liyanghao19890724@163.com>
2020-04-22 06:55:18 +0800

    Create p3.c

---
+++ projects/chapter6/p3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void){
+
+	int numerator, denominator;
+
+	printf("Enter a fraction: ");
+	scanf("%d/%d", &numerator, &denominator);
+
+	int m = numerator;
+	int n = denominator;
+
+	if (n > m) {
+		int temp = n;
+		n = m;
+		m = temp;
+	}
+
+	int gcd = m;
+	while (n != 0) {
+		int remainder = m % n;
+		m = n;
+		n = remainder;
+		gcd = m;
+	}
+
+	printf("In lowest form: %d/%d\n", numerator/gcd, denominator/gcd);
+
+	return 0;
+}
