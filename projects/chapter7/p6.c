6a10addc93bf52af7e67fa94dc3d8c347d6d9eee
LiyangHao <liyanghao19890724@163.com>
2020-04-26 17:52:09 +0800

    Create p6.c

---
+++ projects/chapter7/p6.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+
+    printf("The value of sizeof(int) is %lu\n", sizeof(int));
+    printf("The value of sizeof(short) is %lu\n", sizeof(short));
+    printf("The value of sizeof(long) is %lu\n", sizeof(long));
+    printf("The value of sizeof(float) is %lu\n", sizeof(float));
+    printf("The value of sizeof(double) is %lu\n", sizeof(double));
+    printf("The value of sizeof(long double) is %lu\n", sizeof(long double));
+    
+    return 0;
+}
