6493750b6b9daad89fb3a4fb34fe0ab3b73927f9
LiyangHao <liyanghao19890724@163.com>
2020-04-30 05:30:40 +0800

    Update p14.c

--- projects/chapter7/p14.c
+++ projects/chapter7/p14.c
@@ -1,15 +1,33 @@
 #include <stdio.h>
+#include <math.h>
+
+// Enter a positive number: 3  
+// y_old is 1.00000, x/y is 3.00000, average is 2.00000
+// y_old is 2.00000, x/y is 1.50000, average is 1.75000
+// y_old is 1.75000, x/y is 1.71429, average is 1.73214
+// y_old is 1.73214, x/y is 1.73196, average is 1.73205
+// y_old is 1.73205, x/y is 1.73205, average is 1.73205
+// Square root: 1.73205
+
 
 int main(void)
 {	
-	float number;
-	float root;
-	float x, y;
+	double x, y;
+	double mid, y_old;
 
 	printf("Enter a positive number: ");
-	scanf("%f", &number);
+	scanf("%lf", &x);
 
-	root = 1;
-	printf("Square root: %f\n", root);
+	y = 1.0;
+	y_old = 0.0;
+
+	while (fabs(y-y_old)>=y*0.00001){
+		y_old = y;
+		mid = x/y;
+		y = (mid+y)/2;
+		// printf("y_old is %.5f, x/y is %.5f, average is %.5f\n", y_old, mid, y);
+	}
+
+	printf("Square root: %.5f\n", y);
     return 0;
 }
