09c2f604b18b34c198d836ab9c71f3aae44ce459
LiyangHao <liyanghao19890724@163.com>
2020-04-30 06:15:25 +0800

    Create e3.c

---
+++ exercises/chapter8/e3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+typedef int Bool //使用类型定义来设置布尔类型 
+#define TRUE 1
+#define FALSE 0
+
+//在C89中定义布尔类型数组
+int main(void)
+{
+	Bool weeken[7] = {TRUE,FALSE,FALSE,FALSE,FALSE,FALSE,TRUE};
+
+
+	return 0;
+}
