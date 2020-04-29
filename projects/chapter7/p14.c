ed9fcfbcc878cdbe4709075992e6cb94cb5e6459
Liyanghao <liyanghao19890724@163.com>
2020-04-29 18:58:35 +0800

    Create p14.c

---
+++ projects/chapter7/p14.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{	
+	float number;
+	float root;
+	float x, y;
+
+	printf("Enter a positive number: ");
+	scanf("%f", &number);
+
+	root = 1;
+	printf("Square root: %f\n", root);
+    return 0;
+}
