3054a667e11af1af8ff05dbdad1aa6f689d46574
LiyangHao <liyanghao19890724@163.com>
2020-05-01 06:30:20 +0800

    Update e5.c

--- exercises/chapter8/e5.c
+++ exercises/chapter8/e5.c
@@ -9,7 +9,7 @@ int main(void)
 
 	printf("%d,%d", fib_numbers[0], fib_numbers[1]);
 
-	for (i=2;i<=40;i++) {
+	for (i=2;i<40;i++) {
 		fib_numbers[i] = fib_numbers[i-1]+fib_numbers[i-2];
 		printf(",%d", fib_numbers[i]);
 	}
