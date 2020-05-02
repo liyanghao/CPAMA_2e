60aac43452f8422380bc692d8c9e88d43287a13b
LiyangHao <liyanghao19890724@163.com>
2020-05-02 16:56:26 +0800

    Update p1.c

---
+++ projects/chapter8/p1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void) {
+    //使用初始化式来初始化数组，且初始化式的长度小于数组的长度
+    bool digit_seen[10] = {false};
+    int digit_repeated[10] = {0};
+    int digit, i;
+    long n;
+
+    printf("Enter a number: ");
+    scanf("%ld", &n);
+
+    while(n > 0){
+        digit = n%10;
+        if (digit_seen[digit]) {
+            digit_repeated[digit]++;
+        }else {
+            digit_seen[digit] = true;
+        }
+        n /= 10;
+    }
+
+    printf("Repeated digit(s): ");
+    for (i=0;i<10;i++) {
+        if (digit_repeated[i] > 0) {
+            printf("%d ", i);
+        }
+    }
+    printf("\n");
+}
