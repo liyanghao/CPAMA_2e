32b8af3556d79f4e542134f6e62a556fc9a1fa77
LiyangHao <liyanghao19890724@163.com>
2020-05-02 17:13:10 +0800

    Update p2.c

--- projects/chapter8/p2.c
+++ projects/chapter8/p2.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+// Enter a number: 41270192
+// Digit:      0   1   2   3   4   5   6   7   8   9   
+// Occurrences: 1  2   2   0   1   0   0   1   0   1   
+
 int main(void) {
     //使用初始化式来初始化数组，且初始化式的长度小于数组的长度
     bool digit_seen[10] = {false};
